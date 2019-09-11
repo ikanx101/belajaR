010cc99e6efb288a5c47e3105a1b280b6ac6ffbf
ikanx101 <54991077+ikanx101@users.noreply.github.com>
2019-09-11 13:31:18 +0700

    Update KPK.R

--- Bukan Infografis/KPK.R
+++ Bukan Infografis/KPK.R
@@ -20,15 +20,16 @@ target = c('Anggota DPR dan DPRD',
            'Eselon I / II / III',
            'Swasta','Walikota/Bupati dan Wakil')
 windows()
-png('big fish KPK.png',width = 1000,height = 600,res=100)
+png('big fish KPK.png',width = 1500,height = 750,res=100)
 data %>% filter(jabatan %in% target) %>%
-  ggplot(aes(x=(tahun),y=value)) + geom_line(aes(color=jabatan),size=1) + 
+  ggplot(aes(x=(tahun),y=value)) + geom_line(aes(linetype=jabatan,color=jabatan),size=1) + 
   scale_x_continuous(breaks = c(min(data$tahun):max(data$tahun))) +
   scale_color_brewer(palette = 'Set1') +
   labs(x='Tahun ke tahun',y='Banyaknya orang yang ditangkap') + theme_clean() +
-  labs(color='Jabatan') +
+  labs(color='Jabatan',linetype='Jabatan') +
   labs(title='Siapa saja `big fish` yang sering ditangkap KPK setiap tahunnya?',subtitle = 'source: situs KPK') +
   theme(plot.subtitle = element_text(face='italic'),plot.title = element_text(size=20)) +
   theme(axis.title.y = element_text(size=13,face='bold')) +
   theme(axis.title.x = element_text(size=13,face='bold'))
 dev.off()
+#https://passingthroughresearcher.wordpress.com
