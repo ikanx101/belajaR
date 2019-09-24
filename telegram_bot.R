05846953d81d947c6f1cac0266a384ca72f79da8
ikanx101 <54991077+ikanx101@users.noreply.github.com>
2019-09-24 17:15:55 +0700

    tambahan karhutla

--- telegram_bot.R
+++ telegram_bot.R
@@ -1,8 +1,11 @@
 rm(list=ls())
+setwd('D:/Project_R/TelegramBOT')
 library(telegram.bot)
 library(telegram)
 library(readxl)
 library(rvest)
+library(ggplot2)
+library(dplyr)
 updater <- Updater(token = bot_token("RTelegramBot"))
 bot <- TGBot$new(token = bot_token('RTelegramBot'))
 
@@ -12,14 +15,18 @@ print(bot$getMe())
 # Get updates
 update <- bot$getUpdates()
 
+##########################
+#start
 start <- function(bot, update){
   bot$sendMessage(chat_id = update$message$chat_id,
-                  text = paste('Hai Kak ',update$message$from$first_name,', perkenalkan saya i.k.a.n.x_bot_ver_1.5\nSilakan tanyakan tentang machine learning, artificial intelligence, atau apapun itu ya... #projectiseng\nContoh: Apa itu machine learning?\nCari tahu harga emas saat ini, ketik /emas',sep=''))
+                  text = paste('Hai Kak ',update$message$from$first_name,', perkenalkan saya i.k.a.n.x_bot_ver_1.1\nSilakan tanyakan tentang machine learning, artificial intelligence, atau apapun itu ya... #projectiseng\nContoh: Apa itu machine learning?\nCari tahu harga emas saat ini, ketik /emas',sep=''))
 }
 
 start_handler <- CommandHandler('start', start)
 updater <- updater + start_handler
 
+##########################
+#harga emas
 cari <- function(bot, update){
   url='https://harga-emas.org/1-gram/'
   txt = read_html(url) %>% html_nodes('td') %>% html_text()
@@ -29,6 +36,8 @@ cari <- function(bot, update){
 start_handler <- CommandHandler('emas', cari)
 updater <- updater + start_handler
 
+##########################
+#kurs rupiah
 cari <- function(bot, update){
   url='https://harga-emas.org/1-gram/'
   txt = read_html(url) %>% html_nodes('td') %>% html_text()
@@ -38,8 +47,40 @@ cari <- function(bot, update){
 start_handler <- CommandHandler('rupiah', cari)
 updater <- updater + start_handler
 
+
 ##########################
-#Trial detik.com
+#karhutla
+cari <- function(bot, update){
+  url='http://sipongi.menlhk.go.id/hotspot/matrik_tahunan'
+  data = read_html(url) %>% html_table(fill=T)
+  data = data[[1]]
+  data = reshape2::melt(data,id.vars='Provinsi')
+  data$value = as.numeric(data$value)
+  prov = data %>% filter(variable=='Sep') %>% filter(!is.na(value)) %>% arrange(value) %>% tail(5)
+  prov = prov$Provinsi
+  bulan = c('Jul','Agu','Sep')
+  judul = print(Sys.time())
+  judul = gsub(' +07','',judul,fixed = T)
+  data %>% filter(variable %in% bulan) %>% filter(Provinsi %in% prov) %>% ggplot(aes(x=variable,y=value,group=Provinsi)) +
+    theme_minimal() + 
+    geom_line(aes(color=Provinsi,linetype=Provinsi),size=1.5) + geom_label(aes(label=value,color=Provinsi),size=3) +
+    labs(title = 'TOP 5 Provinsi dengan Titik Panas Terbanyak',subtitle=paste('source: SIPONGI KemenLHK pada ',judul,sep='')) +
+    theme(axis.title.x = element_blank(),axis.text.y = element_blank()) + labs(y='Banyak Titik Panas') +
+    theme(plot.title = element_text(face='bold',size=20),plot.subtitle = element_text(face='italic',size=13)) +
+    labs(caption = 'Scrapped Real Time using R\ni k A n X')
+  ggsave('sipongi.png',width = 10,height = 4.5,dpi = 450)
+  bot$sendPhoto(
+    chat_id = update$message$chat_id,
+    photo = 'D:/Project_R/TelegramBOT/sipongi.png',
+    caption = "Data Real Time dari SIPONGI KemenLHK"
+  )
+}
+start_handler <- CommandHandler('karhutla', cari)
+updater <- updater + start_handler
+
+
+##########################
+#detik.com
 cari <- function(bot, update){
   url='https://www.detik.com/'
   webpage <- read_html(url)
@@ -63,8 +104,6 @@ start_handler <- CommandHandler('detik', cari)
 updater <- updater + start_handler
 ##########################
 
-
-
 tolong.donk = function(judul){
   judul=tolower(judul)
   judul=gsub('_','',judul)
@@ -78,49 +117,22 @@ tolong.donk = function(judul){
   return(judul)
 }
 
+#Fungsi data grepl
 start_handler <- function(bot, update){
   text <- "Saya masih belum belajar itu... Maklum #projectiseng, mau bantu develop bot ini? hehehe.\nTry: `Apa itu machine learning?`\n Try: `Apa itu algoritma?`\natau ketik: /start /emas /rupiah /detik\n\n\natau silakan berkunjung ke blog saya di: https://passingthroughresearcher.wordpress.com/"
-  if (grepl('mean',tolong.donk(update$message$text),ignore.case = T)){text = "Salah satu ukuran pemusatan data. Biasa disebut dengan rata - rata. Dihitung dengan membagi jumlah data dengan banyaknya data. Contoh kasus: https://passingthroughresearcher.wordpress.com/2019/01/01/market-research-101-analisa-punya-point-of-view-lhoo/"}
-  else if (grepl('belajar',tolong.donk(update$message$text),ignore.case = T)){text = "Ada berbagai macam cara untuk belajar machine learning. Bisa ambil online course atau hands on dan melihat forum tanya jawab seperti stackoverflow. Tapi pertanyaan yang lebih mendasar adalah `kapan saya harus mulai belajar machine learning?`. Silakan membaca tulisan lengkap saya di: https://passingthroughresearcher.wordpress.com/2018/12/18/kapan-saya-harus-belajar-machine-learning"}
-  else if (grepl('median',tolong.donk(update$message$text),ignore.case = T)){text = "Salah satu ukuran pemusatan data. Biasa disebut dengan nilai tengah. Dicari dengan mengurutkan data dari terkecil ke terbesar lalu lihat di mana tengahnya. Contoh kasus: https://passingthroughresearcher.wordpress.com/2019/01/01/market-research-101-analisa-punya-point-of-view-lhoo/"}
-  else if (grepl('modus',tolong.donk(update$message$text),ignore.case = T)){text = "Salah satu ukuran pemusatan data. Dicari dengan cara melihat data yang paling sering muncul. Biasanya kita sering mendengarkan modus dalam kata: `mayoritas`. Contoh kasus: https://passingthroughresearcher.wordpress.com/2019/01/01/market-research-101-analisa-punya-point-of-view-lhoo/"}
-  else if (grepl('machine',tolong.donk(update$message$text),ignore.case = T)){text = "Secara definisi, machine learning adalah: method of data analysis that automates analytical model building. Kalau saya pribadi lebih senang menyebutnya computational science and statistics. Jadi kalau ada komputasi yang rumit dan melelahkan bagi manusia, biar mesin saja yang melakukannya.\n\nBisa buat apa aja sih?\nMacem-macem yah, dari mulai analisa data (structured atau unstructured) sampai webscrapping data.\n\nSilakan berkunjung ke blog saya di: https://passingthroughresearcher.wordpress.com/ \n\nTry: Bagaimana cara belajarnya?"}
-  else if (grepl('ml',tolong.donk(update$message$text),ignore.case = T)){text = "Secara definisi, machine learning adalah: method of data analysis that automates analytical model building. Kalau saya pribadi lebih senang menyebutnya computational science and statistics. Jadi kalau ada komputasi yang rumit dan melelahkan bagi manusia, biar mesin saja yang melakukannya. \n\nBisa buat apa aja sih?\nMacem-macem yah, dari mulai analisa data (structured atau unstructured) sampai webscrapping data.\n\nSilakan berkunjung ke blog saya di: https://passingthroughresearcher.wordpress.com/ \n\nTry: Bagaimana cara belajarnya?"}
-  else if (grepl('artificial',tolong.donk(update$message$text),ignore.case = T)){text = "Secara definisi, artificial intelligence adalah kecerdasan buatan. Terms ini pertama muncul pada tahun 1950-an. Merupakan umbrella terms dari berbagai related fields yang digunakan untuk memecahkan masalah komputasi seperti matematika, statistik, dan engineering. Pada dasarnya artificial intelligence saat ini adalah kumpulan algoritma yang ditulis oleh manusia untuk mengerjakan well-defined-tasks (paling sering digunakan untuk repetitive tasks). Istilah machine learning dan deep learning termasuk ke dalam AI itu sendiri. Seringkali orang mempersepsikan AI sebagai suatu hal yang sangat luar biasa (bisa melakukan banyak hal secara sendiri), namun demikian tidak seperti itu.Ada dua tools / bahasa yang sering digunakan, yakni R dan Pyton. Mau tau lebih lanjut? Ketik:\nR atau\nPyton"}
-  else if (grepl('algoritma',tolong.donk(update$message$text),ignore.case = T)){text = "Secara simpel algoritma berarti proses berpikir. Bisa dituliskan sebagai baris perintah atau workflow. Sekarang ini kata algoritma erat diasosiasikan sebagai kumpulan baris perintah yang kemudian akan dijalankan oleh mesin atau robot."}
-  else if (tolong.donk(update$message$text) == "r"){text = "R atau GNU R adalah software open source yang digunakan dalam computational science. Biasa digunakan oleh matematikawan dan statistikawan untuk melakukan data analisis. Tidak cuma itu, R juga bisa digunakan untuk melakukan pekerjaan lain seperti web scrapping, video analyzing, image data processing, geolocation analysis, bikin chatbot (seperti bot ini) dll. Silakan masuk ke blog saya di https://passingthroughresearcher.wordpress.com/ untuk contoh kasus yang digunakan dengan R."}
-  else if (tolong.donk(update$message$text) == "pyton"){text = "Pyton pada awalnya digunakan developer untuk membuat software atau aplikasi. Namun sekarang ini digunakan untuk membuat algoritma dan analisa data. Banyak perdebatan mengenai siapa yang terbaik antara R dan Pyton. Tapi saya rasa keduanya memiliki pangsa pasar dan tujuan masing-masing."}
-  else if (grepl('kasih',tolong.donk(update$message$text),ignore.case = T)){text = "Terima kasih kembali... n_n"}
-  else if (grepl('trims',tolong.donk(update$message$text),ignore.case = T)){text = "Terima kasih kembali... n_n"}
-  else if (grepl('thx',tolong.donk(update$message$text),ignore.case = T)){text = "Terima kasih kembali... n_n"}
-  else if (grepl('thank',tolong.donk(update$message$text),ignore.case = T)){text = "Terima kasih kembali... n_n"}
-  else if (grepl('thanx',tolong.donk(update$message$text),ignore.case = T)){text = "Terima kasih kembali... n_n"}
-  else if (tolong.donk(update$message$text) == "sample"|tolong.donk(update$message$text) == "sampel"){text = "Tentang sample, apa yang mau Anda ketahui?\nDefinisi sampel\nCara menghitung banyaknya sampel\nTeknik sampling"}
-  else if (tolong.donk(update$message$text) == "hi"|tolong.donk(update$message$text) == "hai"|tolong.donk(update$message$text) == "halo"){text = "hai, ada yang bisa dibantu kah?\nCoba tuliskan: mean"}
-  else if (tolong.donk(update$message$text) == "hii"|tolong.donk(update$message$text) == "test"|tolong.donk(update$message$text) == "tes"){text = "hai, ada yang bisa dibantu kah?\nCoba tuliskan: artificial intelligence"}
-  else if (tolong.donk(update$message$text) == "hallo"|tolong.donk(update$message$text) == "haloo"|tolong.donk(update$message$text) == "helo"){text = "hai, ada yang bisa dibantu kah?\nCoba tuliskan: sample"}
-  else if (tolong.donk(update$message$text) == "ya"|tolong.donk(update$message$text) == "hellow"|tolong.donk(update$message$text) == "kang"){text = "hai, ada yang bisa dibantu kah?\nCoba tuliskan: modus"}
-  else if (tolong.donk(update$message$text) == "ikang"){text = "hai, ada yang bisa dibantu kah?\nCoba tuliskan: median"}
-  else if (grepl('salam',tolong.donk(update$message$text),ignore.case = T)){text = "Wa alaikum Salam"}
-  else if (tolong.donk(update$message$text) == "assw"){text = "Wa alaikum Salam"}
-  else if (tolong.donk(update$message$text) == "asw"){text = "Wa alaikum Salam"}
-  else if (tolong.donk(update$message$text) == "ass"){text = "Wa alaikum Salam"}
-  else if (tolong.donk(update$message$text) == "pa"){text = "Kenapa San?"}
-  else if (tolong.donk(update$message$text) == "kamu siapa"){text = "Saya chatbot versi #projectiseng ya... Semoga semakin lama bisa semakin pintar. Aamiin..."}
-  else if (tolong.donk(update$message$text) == "kamu apa"){text = "Saya chatbot versi #projectiseng ya... Semoga semakin lama bisa semakin pintar. Aamiin..."}
-  else if (tolong.donk(update$message$text) == "km siapa"){text = "Saya chatbot versi #projectiseng ya... Semoga semakin lama bisa semakin pintar. Aamiin..."}
-  else if (tolong.donk(update$message$text) == "km apa"){text = "Saya chatbot versi #projectiseng ya... Semoga semakin lama bisa semakin pintar. Aamiin..."}
-  else if (tolong.donk(update$message$text) == "siapa kamu"){text = "Saya chatbot versi #projectiseng ya... Semoga semakin lama bisa semakin pintar. Aamiin..."}
-  else if (tolong.donk(update$message$text) == "siapa km"){text = "Saya chatbot versi #projectiseng ya... Semoga semakin lama bisa semakin pintar. Aamiin..."}
-  else if (tolong.donk(update$message$text) == "apa kamu"){text = "Saya chatbot versi #projectiseng ya... Semoga semakin lama bisa semakin pintar. Aamiin..."}
-  else if (tolong.donk(update$message$text) == "apa km"){text = "Saya chatbot versi #projectiseng ya... Semoga semakin lama bisa semakin pintar. Aamiin..."}
-  else if (tolong.donk(update$message$text) == "oh gtu ya"){text = "Iya Kak... Kakak mau tanya apa? yang gampang-gampang aja ya Kak..."}
-  else if (tolong.donk(update$message$text) == "oh gitu ya"){text = "Iya Kak... Kakak mau tanya apa? yang gampang-gampang aja ya Kak..."}
-  else if (tolong.donk(update$message$text) == "oh gt ya"){text = "Iya Kak... Kakak mau tanya apa? yang gampang-gampang aja ya Kak..."}
-  else if (tolong.donk(update$message$text) == "oh gt y"){text = "Iya Kak... Kakak mau tanya apa? yang gampang-gampang aja ya Kak..."}
+  data = read_excel('bank data.xlsx',sheet = 'grepl')
+  for(i in 2:length(data$v1)){
+    if(grepl(data$v1[1],tolong.donk(update$message$text),ignore.case = T)){text=data$v2[1]}
+    else if (grepl(data$v1[i],tolong.donk(update$message$text),ignore.case = T)){text=print(data$v2[i])}
+  }
+  datum = read_excel('bank data.xlsx',sheet = 'ifelse')
+  for(i in 2:length(datum$v1)){
+    if(tolong.donk(update$message$text) == datum$v1[1]){text=datum$v2[1]}
+    else if (tolong.donk(update$message$text) == datum$v1[i]){text=print(datum$v2[i])}
+  }
   bot$sendMessage(chat_id = update$message$chat_id, text = text)
+  bot$sendMessage(chat_id = 826276159, text = update$message$text)
 }
-
 updater <- updater + MessageHandler(start_handler, MessageFilters$text)
-print('Versi 1.5')
+
 updater$start_polling()
