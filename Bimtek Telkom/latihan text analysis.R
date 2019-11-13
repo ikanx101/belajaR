d313535651b9e9db5b79c285a54d11247bcda03b
ikanx101 <ikanx101@gmail.com>
2019-11-13 02:54:16 +0000

    sudah dengan wordcloud

--- Bimtek Telkom/latihan text analysis.R
+++ Bimtek Telkom/latihan text analysis.R
@@ -1,8 +1,32 @@
 rm(list=ls())
+setwd("/cloud/project/Bimtek Telkom")
 
 #libraries
 library(dplyr)
 library(tidyr)
 library(tidytext)
 library(ggplot2)
+library(wordcloud2)
 
+#yuk mari kita mulai yah
+string01 = "Budi dan Badu bermain bola di sekolah"
+string02 = "Apakah Romi dan Julia saling mencintai saat mereka berjumpa di persimpangan jalan?"
+
+#kita bikin tibbel dari data ini yah
+data = tibble(
+  id = c(1,2),
+  string = c(string01,string02)
+)
+View(data)
+
+#kita mulai bikin wordcloud yah
+wc = 
+  data %>% mutate(string = tolower(string)) %>%
+  unnest_tokens(words,string) %>% count(words,sort = T)
+
+wordcloud2(wc,
+           color = "random-dark", 
+           backgroundColor = "white",
+           shape = 'cardioid',
+           fontFamily = "Miso",
+           size=2)
