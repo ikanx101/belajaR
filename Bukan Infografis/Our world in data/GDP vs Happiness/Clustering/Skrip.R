a658bca67050064ea4766af1c0f4372f5e810de7
ikanx101 <ikanx101@gmail.com>
2019-11-20 23:00:21 +0000

    Clustering

--- Bukan Infografis/Our world in data/GDP vs Happiness/Clustering/Skrip.R
+++ Bukan Infografis/Our world in data/GDP vs Happiness/Clustering/Skrip.R
@@ -6,69 +6,55 @@ library(ggplot2)
 library(tidyverse)
 library(ggpubr)
 library(ggthemes)
+library(factoextra)
 
 #baca data
 file = list.files()
-data = read.csv(file[1])
+data = read.csv(file[2])
 str(data)
 colnames(data) = c('negara','code','tahun',
                    'gdp.per.capita',
                    'life.satisfaction',
                    'population')
 
-#kita fokus di tahun 2017
-data %>% filter(tahun==2017) %>% 
+#kita mulai clusteringnya yah
+#dimulai dari mengambil data dan variabel yang diinginkan
+data.new.1 =
+  data %>% filter(tahun==2017) %>%
   filter(!is.na(gdp.per.capita)) %>%
   filter(!is.na(life.satisfaction)) %>%
   filter(!is.na(population)) %>%
-  ggplot(aes(x = gdp.per.capita,
-             y = life.satisfaction)) +
-  theme_tufte() +
-  geom_point(aes(size = population,color = population),alpha=.7) +
-  geom_text(aes(label = negara),size=2.25,alpha=.45) +
-  geom_smooth(method = 'lm',alpha=.1) +
-  theme(legend.position = 'none') +
-  labs(title = 'Money Can`t Buy Happiness...\nIs that true?',
-       subtitle = 'Each dot in the visualization represents one country. The vertical position of the dots shows national average self-reported life satisfaction\nin the Cantril Ladder (a scale ranging from 0-10 where 10 is the highest possible life satisfaction)\nwhile the horizontal position shows GDP per capita based on purchasing power parity (i.e. GDP per head after adjusting for inflation and cross-country price differences).',
-       caption = 'Scraped and Visualized from ourworldindata.org\nusing R\ni k A n x',
-       x = 'GDP per Capita',
-       y = 'Life satisfaction') +
-  theme(axis.text = element_blank(),
-        plot.title = element_text(size=24,face='bold'),
-        plot.subtitle = element_text(size=10),
-        plot.caption = element_text(size=8,face='bold.italic'),
-        axis.title = element_text(size=15,face='bold')) +
-  stat_regline_equation(
-    aes(label =  paste(..eq.label.., ..adj.rr.label.., sep = "~~~~"))
-  )
-ggsave('reg model 2.png',
-       width = 10,
-       height = 8,
-       dpi = 600)
+  mutate(negara = as.character(negara)) 
+head(data.new.1)
 
-#kita lagi dengan tambahan korelasi yah
-korel =
-  data %>% filter(tahun==2017) %>%
-  filter(!is.na(gdp.per.capita)) %>%
-  filter(!is.na(life.satisfaction)) %>%
-  filter(!is.na(population)) %>% 
-  summarise(cor(gdp.per.capita,life.satisfaction))
-# ditemukan korelasi yang tinggi
-write.table(korel,file='Hasil korelasi.txt')
+data.new =
+  data.new.1 %>%
+  select(gdp.per.capita,life.satisfaction,population)
+setwd("/cloud/project/Bukan Infografis/Our world in data/GDP vs Happiness/Clustering")
 
-#kita bikin modelnya yah
-data.lm =
-  data %>% filter(tahun==2017) %>%
-  filter(!is.na(gdp.per.capita)) %>%
-  filter(!is.na(life.satisfaction)) %>%
-  filter(!is.na(population))
+#transformasi skala
+data.new =
+  data.new %>%
+  mutate(gdp.per.capita = cut(gdp.per.capita,breaks = 5,labels = c(1,2,3,4,5)),
+         life.satisfaction = cut(life.satisfaction,breaks = 5,labels=c(1,2,3,4,5)),
+         population = cut(population,breaks = 5,labels = c(1,2,3,4,5)))
+head(data.new)
 
-model.reg =
-  lm(life.satisfaction~gdp.per.capita,
-     data=data.lm)
-summary(model.reg)
-str(model.reg)
+#kita pake elbow yah
+elbow=fviz_nbclust(data.new, kmeans, method = "wss") 
+ggsave(elbow,file='elbow method.png',width=16,height=9, units='in',dpi=150)
 
-sink('summary reg model.txt')
-print(summary(model.reg))
+#yang ini pake siluet
+siluet=fviz_nbclust(data.new, kmeans, method = "silhouette") #siluet method hasil: 2 dan 6
+ggsave(siluet,file='sillhoutte method.png',width=16,height=9, units='in',dpi=150)
+
+head(data.new)
+set.seed(123)
+final = kmeans(data.new,7,nstart=10)
+final$cluster
+data.new.1$cluster = final$cluster
+head(data.new.1)
+
+sink('hasil clustering.txt')
+print(final)
 sink()
