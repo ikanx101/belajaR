e91d07c2119ab9b41c85a91c485f1b64cc39113c
ikanx101 <ikanx101@gmail.com>
2019-11-28 01:11:10 +0000

    Draft

--- nitip sebentar/nitip dulu/skrip.R
+++ nitip sebentar/nitip dulu/skrip.R
@@ -13,25 +13,45 @@ doc = read_pptx('template.pptx')
 # bikin slide pertama
 tambahin.slide.judul.donk('Time Series Decomposition','Granger Causality')
 
-#kita ambil datanya
+# kita ambil datanya
 url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRnPxduyaDGHSqR_Wz39y_bSsN-4rO4hZQoRkd6HXNviSw4eEE3mp71kzdmm2eDbxIolzWaPIMyYtMB/pubhtml'
 data = read_html(url) %>% html_table()
 data = data[[1]]
 colnames(data) = data[1,]
 data = data[-1,]
 
+# all market all parent
 tes = 
   data %>%
-  group_by(parent,brand,tahun) %>%
-  summarise(sum(as.numeric(value))) %>%
-  arrange(brand)
+  mutate(bulan = as.numeric(bulan),
+         tahun = as.numeric(tahun)) %>%
+  filter(tahun<=2018) %>%
+  group_by(bulan,tahun) %>%
+  summarise(value = sum(as.numeric(value))) %>%
+  arrange(tahun,bulan)
+View(tes)
 
-ts=ts(dummy$sales,start=c(min(dummy$year),1),frequency=12)
-decompose=stl(ts,s.window='periodic')
+tambahin.slide.pemisah.donk('Market Trends',
+                            'All Parents dan All Brands')
+
+all = ts(tes$value,start=c(2014,1),frequency = 12)
+all
+str(all)
+summary(all)
+decompose=stl(all,s.window='periodic')
+decompose
 decompose=data.frame(decompose$time.series)
-dummy$seasonal=decompose$seasonal
-dummy$trend=decompose$trend
-dummy$remainder=decompose$remainder
+seasonal=decompose$seasonal
+trend=decompose$trend
+remainder=decompose$remainder
+
+hasil = data.frame(seasonal,trend,decompose,
+                   bulan = tes$bulan,
+                   tahun = tes$tahun)
+hasil %>% ggplot(aes(x=paste(bulan,tahun,sep='-'),
+                     y=trend)) +
+  geom_line(group=1) + theme(axis.text.x = element_text(angle=90))
+ggsave('Decompose.png')
 
 tambahin.slide.ending.donk()
 export.powerpoint.saya.donk('hasil sementara')
