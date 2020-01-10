d769f89b1dca6095df35da21e1ce22df2b3e5144
ikanx101 <ikanx101@gmail.com>
2020-01-10 06:37:07 +0000

    SMAN 1 Bekasi

--- Bukan Infografis/Urunan Eka/urunan eka.R
+++ Bukan Infografis/Urunan Eka/urunan eka.R
@@ -27,25 +27,45 @@ chart_1 =
   ggplot(aes(x=id,y=saldo,color=tanggal)) + geom_line() +
   geom_text_repel(aes(label = paste(round(saldo/1000000,2),'juta',sep=' ')),size=3) +
   theme_minimal() +
-  labs(title='Penambahan Saldo Urunan utk Eka',
+  labs(title='Laporan Penambahan Saldo Urunan',
        subtitle='SMAN 1 Bekasi - 2004',
-       caption='last update: 10 Jan 2020 11:12',
+       caption='last update: 10 Jan 2020 11:30',
        color='Tanggal:') +
   theme(axis.title = element_blank(),
         axis.ticks = element_blank(),
         axis.text = element_blank(),
         plot.title = element_text(size=20,face='bold.italic'))
+#chart_1
 
 # kita buat chart kedua
 chart_2 = 
   data %>% select(tanggal,jumlah) %>% group_by(tanggal) %>%
-  summarise(jumlah = sum(jumlah)) %>%
+  summarise(jumlah = sum(jumlah)) %>% 
+  mutate(tanggal=factor(tanggal,levels = c('8 Jan','9 Jan','10 Jan'))) %>%
   ggplot(aes(x=tanggal,y=jumlah)) + geom_col(color='steelblue',fill='white') +
   theme_minimal() +
-  labs(title = 'Saldo Harian Urunan utk Eka',
-       subtitle = 'SMAN 1 Bekasi - 2004') +
-  geom_label(aes(label = round(jumlah/1000000,2)),size=4) +
+  labs(title = 'Penambahan Saldo Harian') +
+  geom_label(aes(label = paste(round(jumlah/1000000,2),'juta',sep=' ')),size=4) +
   theme(axis.title = element_blank(),
         axis.text.y = element_blank(),
-        plot.title = element_text(size=13,face='bold.italic'))
-chart_2  
+        plot.title = element_text(size=10,face='bold.italic'))
+#chart_2  
+
+# kita buat chart ketiga
+chart_3 = 
+  data %>% select(tanggal,jumlah) %>% group_by(tanggal) %>%
+  summarise(jumlah = n()) %>% 
+  mutate(tanggal=factor(tanggal,levels = c('8 Jan','9 Jan','10 Jan'))) %>%
+  ggplot(aes(x=tanggal,y=jumlah)) + geom_col(color='steelblue',fill='white') +
+  theme_minimal() +
+  labs(title = 'Transaksi Masuk') +
+  geom_label(aes(label = paste(jumlah,'trx',sep=' ')),size=4) +
+  theme(axis.title = element_blank(),
+        axis.text.y = element_blank(),
+        plot.title = element_text(size=10,face='bold.italic'))
+#chart_3  
+
+gabung_1 = ggarrange(chart_3,chart_2,ncol=1,nrow=2)
+gabung = ggarrange(gabung_1,chart_1,ncol=2,nrow=1,widths=c(1,3))
+
+ggsave(gabung,file='final eka.png',width=10,height=7,dpi=150)
