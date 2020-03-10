e535162e71023fdb03e20e2661c6c02f67bf45cb
ikanx101 <54991077+ikanx101@users.noreply.github.com>
2020-03-10 17:02:23 +0700

    revisong

--- Bukan Infografis/Chris Nolan/script.R
+++ Bukan Infografis/Chris Nolan/script.R
@@ -94,6 +94,7 @@ DATA %>%
   labs(title = 'FILM ARAHAN CHRISTOPHER NOLAN',
        subtitle = 'base: aktor dan aktris yang bermain dalam film') +
   theme(plot.title = element_text(size=15,face='bold'))
+ggsave('Chris Nolan.png',width = 600,height=900,dpi=250)
 
 # SNA analisis
 final = DATA[1:2]
@@ -123,6 +124,14 @@ View(analisa.hasil)
 #bikin chart yg sama
 g = graph.adjacency(hasil,weighted = T, mode='undirected')
 g = simplify(g)
+set.seed(3952)
+layout1 = layout.fruchterman.reingold(g)
+png('between.png',width = 1024,height = 768,units = 'px');plot(g, layout=layout1,edge.curved=0.2,vertex.label.cex=1.25,vertex.size=between/2);dev.off()
+png('degree.png',width = 1024,height = 768,units = 'px');plot(g, layout=layout1,edge.curved=0.2,vertex.label.cex=1.25,vertex.size=degree);dev.off()
+
 fc = fastgreedy.community(as.undirected(g))
 V(g)$color <- ifelse(membership(fc)==1,"red","blue")
-plot(g)
+png('cluster membership.png',
+    width = 1024,
+    height = 768,
+    units = 'px');plot(g);dev.off()
