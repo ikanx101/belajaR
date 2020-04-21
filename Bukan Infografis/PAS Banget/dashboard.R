1e677685dc1d668c0653b4badfd7aa2cb103469d
ikanx101 <ikanx101@gmail.com>
2020-04-21 07:51:50 +0000

    all new

--- Bukan Infografis/PAS Banget/dashboard.R
+++ Bukan Infografis/PAS Banget/dashboard.R
@@ -9,15 +9,16 @@ library(dplyr)
 library(ggplot2)
 library(ggpubr)
 
-excel_sheets('Data Orderan.xlsx')
+options(scipen = 99)
 
-data = read_excel('Data Orderan.xlsx',sheet = 'Rekap') 
+
+data = read_excel('Data Orderan_Rev01.xlsx',sheet = 'Rekap') 
 colnames(data) = janitor::make_clean_names(colnames(data)) 
 nama_var = colnames(data)
 data$id = c(1:length(data$tanggal))
 
 data_1 = data %>% select(nama_var[1:6],id)
-data_2 = data %>% select(nama_var[7:23],id) %>%
+data_2 = data %>% select(nama_var[7:length(nama_var)],id) %>%
   mutate_if(is.character,as.numeric)
 
 
@@ -32,33 +33,22 @@ data =
     hari = lubridate::wday(tanggal, label=T)
     )
 
-
 data[is.na(data)] = 0
 
 rekap = 
 data %>% 
   mutate(transfer_cash = ifelse(transfer_cash == 'Cash',
                                 1,
-                                0),
-         total_belanja_212_mart = as.numeric(total_belanja_212_mart),
-         bagi_hasil_ojek_80_percent = as.numeric(bagi_hasil_ojek_80_percent),
+                                0)
          ) %>%
   group_by(tanggal) %>%
   summarise(order = n(),
             n_ojek = length(unique(ojek)),
             pelanggan = length(unique(nama_pelanggan)),
-            pembayaran_cash = round(sum(transfer_cash)/n()*100,1),
-            trans_212 = sum(x212_mart),
-            trans_nusantara = sum(pasar_nusantara),
-            trans_lainnya = sum(other),
-            total_212 = sum(total_belanja_212_mart),
-            total_nusantara = sum(total_belanja_pasar_nusantara),
-            total_others = sum(total_belanja_others),
-            total_all = sum(total_belanja_semua),
-            fee_ojek = sum(bagi_hasil_ojek_80_percent),
-            donasi = sum(donasi_pelanggan)
+            pembayaran_cash = round(sum(transfer_cash)/n()*100,1)
             )
 
+# chart 1 aman
 chart_1 = 
   rekap %>%
   ggplot() +
@@ -88,12 +78,13 @@ chart_1 =
         axis.title.y = element_blank(),
         axis.ticks.y = element_blank())
 
+# aman
 chart_2 = 
   data %>%
   group_by(tanggal) %>%
-  summarise(rata = mean(total_belanja_semua),
-            min = min(total_belanja_semua),
-            max = max(total_belanja_semua)) %>%
+  summarise(rata = mean(total_bayar + tips_pelanggan),
+            min = min(total_bayar + tips_pelanggan),
+            max = max(total_bayar + tips_pelanggan)) %>%
   ggplot() +
   geom_col(aes(x = tanggal,
                y = rata),
@@ -113,29 +104,47 @@ chart_2 =
   theme_minimal() +
   labs(title = 'Rata-Rata Transaksi Belanja Harian',
        x = 'Tanggal',
-       subtitle = 'Garis menunjukkan range (min - max) belanjaan pada hari tersebut') +
+       subtitle = 'Total Bayar plus Tips Pelanggan\nGaris menunjukkan range (min-max) belanjaan pada hari tersebut') +
   theme(axis.text.y = element_blank(),
         axis.title.y = element_blank(),
         axis.ticks.y = element_blank())
 
+
+
+
+
+
 chart_4 =
-  rekap %>%
-  select(tanggal,trans_212,trans_nusantara,trans_lainnya) %>%
+  data %>%
+  select(tanggal,
+         total_pas_mart_pasar_nusantara,
+         total_pas_mart_212_mart,
+         total_pas_mart_own,
+         total_pas_mart_other,
+         total_pas_food,
+         tarif_pas_send) %>%
   reshape2::melt(id.vars = 'tanggal') %>%
-  mutate(variable = case_when(variable == 'trans_212' ~ '212 Mart',
-                              variable == 'trans_nusantara' ~ 'Pasar Nusantara',
-                              variable == 'trans_lainnya' ~ 'Lainnya')) %>%
+  mutate(variable = case_when(variable == 'total_pas_mart_212_mart' ~ '212 Mart',
+                              variable == 'total_pas_mart_pasar_nusantara' ~ 'Pasar Nusantara',
+                              variable == 'total_pas_mart_other' ~ 'Lainnya',
+                              variable == 'total_pas_mart_own' ~ 'PAS Own Mart',
+                              variable == 'total_pas_food' ~ 'PAS Food',
+                              variable == 'tarif_pas_send' ~ 'PAS Send')
+         ) %>%
   rename(toko = variable,
          trans = value) %>%
+  group_by(tanggal,toko) %>%
+  summarise(trans = sum(trans)) %>%
   ggplot(aes(x = tanggal,
              y = trans)) +
   geom_col(aes(fill = toko),
            color = 'steelblue',
            size = .7,
            alpha = .3) +
+  scale_fill_brewer(palette = 'Set1') +
   theme_minimal() +
-  labs(title = 'Berapa Banyak Toko yang Terlibat',
-      subtitle = 'Dalam real number',
+  labs(title = 'Berapa Market Size dari Toko yang Terlibat',
+      subtitle = 'Dalam Rupiah',
       x = 'Tanggal',
       fill = 'Jenis Toko') +
   theme(axis.title.y = element_blank())
@@ -169,7 +178,7 @@ koor_pasar =
 chart_6 = 
   data %>%
   group_by(tanggal,ojek) %>%
-  summarise(donasi = sum(donasi_pelanggan),
+  summarise(donasi = sum(tips_pelanggan),
             fee = sum(bagi_hasil_ojek_80_percent)) %>%
   ungroup() %>%
   mutate(fee_total = donasi + fee) %>%
@@ -197,6 +206,6 @@ chart_6 =
                                         size=1.5, linetype="solid"))
 
 item_1 = ggarrange(chart_1,chart_6,ncol=2,nrow=1,widths = c(1,1.75))
-item_2 = ggarrange(chart_2,chart_4,chart_5,ncol=3,nrow=1)
+item_2 = ggarrange(chart_2,chart_4,chart_5,ncol=3,nrow=1,widths = c(1,1.25,.8))
 ggarrange(item_1,item_2,ncol=1,nrow=2,heights = c(1.25,.8))
 ggsave('pas.png',width = 14,height=6,dpi=500)
