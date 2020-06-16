2b3774e54105f66114b0695c0209876aa6636099
ikanx101 <ikanx101@gmail.com>
2020-06-16 09:16:38 +0700

    new

--- Bukan Infografis/covid jakarta kantor/covid_jakarta/app.R
+++ Bukan Infografis/covid jakarta kantor/covid_jakarta/app.R
@@ -12,17 +12,16 @@ library(tidyr)
 library(readxl)
 library(dplyr)
 library(shinydashboard)
-
+library(ggrepel)
+library(plotly)
 
 # ---------------------------------
 # ambil fungsi
 rm(list=ls())
-source('graph.R')
 
 # ambil data
 load('all files.rda')
-
-
+provinsi = unique(data_covid_provinsi$province)
 
 # ---------------------------------
 # ui
@@ -30,10 +29,27 @@ load('all files.rda')
 header = dashboardHeader(title = "COVID 19 vs TBC di Indonesia",titleWidth = 350)
 sidebar = dashboardSidebar(width = 350,
                            sidebarMenu(
+                               menuItem(tabName = 'filterpane',
+                                        text = 'Filter Pane'),
                                menuItem(tabName = 'covid_detail',
                                         text = 'Covid Pandemi in Indonesia')
-                           ))
+                           )
+                           )
 
+filterpane = tabItem(tabName = 'filterpane',
+                     fluidRow(
+                         column(width = 12,
+                                h1('Filter Pane'),
+                                h3('Silakan pilih filter berikut ini sesuai dengan kebutuhan'))
+                     ),
+                     br(),
+                     fluidRow(
+                         column(width = 4,
+                                checkboxGroupInput('prop','Provinsi di Indonesia',
+                                                   sort(provinsi),selected = provinsi))
+                     )
+                     )
+    
 covid_detail = tabItem(tabName = 'covid_detail',
                        fluidRow(
                            column(width = 12,
@@ -47,11 +63,26 @@ covid_detail = tabItem(tabName = 'covid_detail',
                        ),
                        fluidRow(
                            column(width = 12,
-                                  dataTableOutput('table'))
+                                  h2('Data Detail Sebaran COVID-19 per Provinsi'),
+                                  h4('Update tanggal 15 Juni 2020, pukul 23.59'),
+                                  DT::dataTableOutput("tabel1"))
+                       ),
+                       br(),
+                       fluidRow(
+                           column(width = 5,
+                                  plotOutput('plot1',height = 550)),
+                           column(width = 7,
+                                  plotOutput('plot2',height = 550))
+                       ),
+                       br(),
+                       fluidRow(
+                           column(width = 12,
+                                  h2('Detail Confirmed Cases'),
+                                  plotlyOutput('plot3',height = 600))
                        )
                        )
     
-body = dashboardBody(tabItems(covid_detail))
+body = dashboardBody(tabItems(filterpane,covid_detail))
 
 ui = dashboardPage(skin = "red",header,sidebar,body)
 
@@ -61,13 +92,86 @@ ui = dashboardPage(skin = "red",header,sidebar,body)
 server <- function(input, output) {
     
     # tabel 1
-    output$table <- renderDataTable(DT::datatable(data_covid_provinsi))
+    output$tabel1 <- DT::renderDataTable({
+        data_covid_provinsi %>% 
+            filter(provinsi %in% input$prop)
+    })
     
     # plot 1
     output$plot1 <- renderPlot({
+        data_covid_provinsi = 
+            data_covid_provinsi %>% 
+            filter(provinsi %in% input$prop)
         
+        mean1 = mean(data_covid_provinsi$fatality_rate)
+        mean2 = mean(data_covid_provinsi$recovery_rate)
+        
+        data_covid_provinsi %>%
+            ggplot(aes(x=recovery_rate,
+                       y=fatality_rate)) +
+            geom_point(color = 'black',aes(size = confirmed)) +
+            geom_text_repel(aes(label = province,alpha = confirmed)) +
+            theme_pubclean() +
+            labs(x = 'Recovery Rate',
+                 y = 'Fatality Rate',
+                 title = 'Recovery vs Fatality in Indonesia',
+                 subtitle = 'Point size indicate confirmed cases') +
+            geom_vline(xintercept = mean2, color = 'red') +
+            geom_hline(yintercept = mean1, color = 'red') +
+            theme(legend.position = 'none')
     })
     
+    # plot 2
+    output$plot2 <- renderPlot({
+        data_covid_provinsi = 
+            data_covid_provinsi %>% 
+            filter(provinsi %in% input$prop)
+        
+        data_covid_provinsi %>% 
+            select(province,confirmed,cases_per_mil_pop) %>% 
+            reshape2::melt(id.vars = 'province') %>% 
+            mutate(variable = ifelse(variable == 'confirmed',
+                                     'Confirmed Cases',
+                                     'Cases per Million Population')) %>%
+            ggplot(aes(x = province,
+                       y = value)) +
+            geom_col(color = 'black',fill = 'blue', alpha = .25) + 
+            geom_label(aes(label = value),size=3) +
+            facet_wrap(~variable,nrow = 2,scales = 'free_y') +
+            theme_pubclean() +
+            theme(axis.text.x = element_text(angle=90),
+                  axis.text.y = element_blank()) +
+            theme(strip.background = element_rect(colour="black", 
+                                                  fill="white",
+                                                  size=1.5, 
+                                                  linetype="solid"),
+                  axis.title = element_blank(),
+                  axis.ticks = element_blank())
+    })
+    
+    # plot 3
+    output$plot3 <- renderPlotly({
+        data_covid_provinsi = 
+            data_covid_provinsi %>% 
+            filter(provinsi %in% input$prop)
+        
+        new = 
+            data_covid_provinsi %>% 
+            select(province,deaths,active,recovered) %>% 
+            reshape2::melt(id.vars = 'province') %>% 
+            mutate(text = paste(province,variable,'=',value))
+        
+        chart = 
+        new %>% 
+            ggplot(aes(x = reorder(province,-value),
+                       y = value)) +
+            geom_col(aes(fill = variable,text = text)) +
+            theme(axis.text.x = element_text(angle=90)) +
+            labs(x = 'Provinsi',
+                 y = 'Cases')
+        
+        ggplotly(chart, tooltip = "text")
+    })
 }
 
 # ---------------------------------
--- Bukan Infografis/covid jakarta kantor/covid_jakarta/cleaning.R
+++ Bukan Infografis/covid jakarta kantor/covid_jakarta/cleaning.R
@@ -1,11 +1,13 @@
 rm(list=ls())
+setwd("~/Documents/belajaR/Bukan Infografis/covid jakarta kantor/covid_jakarta")
 
 library(readxl)
 library(dplyr)
+library(rvest)
 
-setwd("~/Documents/belajaR/Bukan Infografis/covid jakarta kantor/covid_jakarta")
-
-load('scrape covid.rda')
+url = 'https://en.wikipedia.org/wiki/COVID-19_pandemic_in_Indonesia'
+data = read_html(url) %>% html_table(fill=T)
+data = data[[5]]
 
 data = janitor::clean_names(data)
 data = data[-1,]
@@ -16,25 +18,28 @@ colnames(data)[4] = 'confirmed'
 colnames(data)[5] = 'recovered'
 colnames(data)[6] = 'deaths'
 colnames(data)[7] = 'active'
+colnames(data)[8] = 'cases_per_mil_pop'
 
+raw = data
 data =
-  data %>% 
+  raw %>% 
   mutate(confirmed = gsub('\\,','',confirmed),
          recovered = gsub('\\,','',recovered),
          deaths = gsub('\\,','',deaths),
          active = gsub('\\,','',active),
-         cases_per_million_population_f = gsub('\\,','',cases_per_million_population_f),
-         recovery_rate = gsub('\\%','',active),
-         fatality_rate = gsub('\\%','',active),
+         cases_per_mil_pop = gsub('\\,','',cases_per_mil_pop),
+         recovery_rate = gsub('\\%','',recovery_rate),
+         fatality_rate = gsub('\\%','',fatality_rate),
          confirmed = as.numeric(confirmed),
          recovered = as.numeric(recovered),
          deaths = as.numeric(deaths),
          active = as.numeric(active),
-         cases_per_million_population_f = as.numeric(cases_per_million_population_f),
+         cases_per_mil_pop = as.numeric(cases_per_mil_pop),
          recovery_rate = as.numeric(recovery_rate),
          fatality_rate = as.numeric(fatality_rate)
   )
 
 data_covid_provinsi = data
-
+data_covid_provinsi$last_update = NULL
+data_covid_provinsi$major_city = NULL
 save(data_covid_provinsi,file='all files.rda')
