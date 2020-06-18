5a1d55d0f0c68470e776281ec317cf71e9118c25
ikanx101 <ikanx101@gmail.com>
2020-06-18 18:48:49 +0700

    kerjain dunia dulu

--- Bukan Infografis/Shiny COVID REAL/realshinycovid/app.R
+++ Bukan Infografis/Shiny COVID REAL/realshinycovid/app.R
@@ -19,53 +19,75 @@ library(plotly)
 
 # ---------------------------------
 # ambil data dan semua fungsi
+rm(list=ls())
+load('all files.rda')
+
+# tanggal
+tanggal = Sys.Date()
+tanggal = as.character(tanggal)
+
+# video update youtube masuk ke sini yah
+url = 'https://www.youtube.com/watch?v=WaF_YcKMF6A'
+url = gsub('https://www.youtube.com/watch?v=','',url,fixed = T)
+url
+
+
+# ---------------------------------
+# USER INTERFACE
+
+# header
+header = dashboardHeader(title = "COVID 19 Indonesia Viz",
+                         titleWidth = 350)
+
+#sidebar menu
+sidebar = dashboardSidebar(width = 350,
+                           sidebarMenu(
+                               menuItem(tabName = 'filterpane',
+                                        text = 'Read Me'),
+                               menuItem(tabName = 'dunia',
+                                        text = 'Data Dunia'),
+                               menuItem(tabName = 'jabar',
+                                        text = 'Data Covid 19 Jawa Barat')
+                                       )
+            )
+
+# tab Read Me
+filterpane = tabItem(tabName = 'filterpane',
+                     fluidRow(
+                         column(width = 12,
+                                h1('Read Me'),
+                                h3('Dashboard visualisasi ini berisi updated data dari kasus Covid 19 di Indonesia dan dunia. Sebagaimana yang kita ketahui bersama, gugus tugas percepatan penanganan Covid 19 selalu mengupdate informasi setiap sore. Oleh karena itu, data pada dashboard ini juga akan di-update pada pukul 18.00 WIB setiap harinya.'),
+                                h5(paste0('Last update: ',tanggal)),
+                                br(),
+                                h3('Bagi yang kangen dengan Pak Yuri, silakan tonton dulu press conference hari ini:'),
+                                tags$iframe(width="560", height="315", src=paste0("https://www.youtube.com/embed/",url), frameborder="0", allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture", allowfullscreen=NA),
+                                h5('courtesy: youtube.com media indonesia')
+                                )
+                     )
+                     )
+
+dunia = tabItem(tabName = 'dunia',
+                fluidRow(column(width = 12,
+                                h1('Data Covid 19 Dunia'),
+                                h3('Covid 19 menjadi pandemi global. Mari kita lihat bagaimana kondisi terupdate dari negara lain.'),
+                                h4('Sumber data: ourworld in data'),
+                                h5(paste0('Last update: ',tanggal)))
+                         )
+                )
+
+# body
+body = dashboardBody(tabItems(filterpane,dunia))
+
+# ui all
+ui = dashboardPage(skin = "red",header,sidebar,body)
 
 
 
 
-
-
-
-
-
-
-
-
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+server <- function(input, output, session) {
 }
 
+
+# ---------------------------------
 # Run the application 
 shinyApp(ui = ui, server = server)
--- Bukan Infografis/Shiny COVID REAL/realshinycovid/preparation/All Prep.R
+++ Bukan Infografis/Shiny COVID REAL/realshinycovid/preparation/All Prep.R
@@ -5,6 +5,10 @@ setwd("~/Documents/belajaR/Bukan Infografis/Shiny COVID REAL/realshinycovid/prep
 library(readxl)
 library(dplyr)
 
+# data dunia
+# sumber url
+# https://ourworldindata.org/coronavirus
+
 data_dunia = read.csv('~/Documents/belajaR/Bukan Infografis/Shiny COVID REAL/realshinycovid/DaMen/owid-covid-data.csv')
 
 data_dunia =
@@ -12,3 +16,59 @@ data_dunia =
   select(location,new_cases,total_cases,new_deaths,total_deaths,total_cases_per_million,total_tests,total_tests_per_thousand,population,
          median_age,gdp_per_capita,diabetes_prevalence)
 
+# data pikobar
+# sumber url
+# https://pikobar.jabarprov.go.id/table-case
+
+data_jabar = readr::read_csv('~/Documents/belajaR/Bukan Infografis/Shiny COVID REAL/realshinycovid/DaMen/Data COVID-19 Jawa Barat.csv')
+data_jabar$X15 = NULL
+
+data_jabar = 
+  data_jabar %>% 
+  select(tanggal,nama_kab,odp,pdp,positif,sembuh,meninggal)
+
+# data kawalcovid19
+# sumber url
+# http://sinta.ristekbrin.go.id/covid/datasets
+# sheet pertama
+data_1 = read_excel('~/Documents/belajaR/Bukan Infografis/Shiny COVID REAL/realshinycovid/DaMen/COVID-19 di Indonesia @kawalcovid19.xlsx',
+                    sheet = 'Statistik Harian')
+data_1 = janitor::clean_names(data_1)
+data_1 = 
+  data_1 %>% 
+  select(x1,kasus_baru,dalam_perawatan,sembuh,meninggal_dunia,pdp,odp,jumlah_orang_diperiksa,
+         negatif,positif_c) %>% 
+  rename(tanggal = x1)
+
+data_1[is.na(data_1)] = 0
+data_nasional_harian = data_1
+
+# sheet kedua
+data_1 = read_excel('~/Documents/belajaR/Bukan Infografis/Shiny COVID REAL/realshinycovid/DaMen/COVID-19 di Indonesia @kawalcovid19.xlsx',
+                    sheet = 'Kasus per Provinsi',
+                    skip = 1)
+data_1 = janitor::clean_names(data_1)
+data_1 = 
+  data_1 %>% 
+  select(provinsi_asal_2,kasus,sembuh,kematian)
+data_1 = data_1[1:34,]
+data_prov_total = data_1
+
+# sheet ketiga
+data_1 = read_excel('~/Documents/belajaR/Bukan Infografis/Shiny COVID REAL/realshinycovid/DaMen/COVID-19 di Indonesia @kawalcovid19.xlsx',
+                    sheet = 'Timeline')
+
+data_1 = read_excel("Bukan Infografis/Cawal Kovid/CawalKovid/COVID-19 di Indonesia @kawalcovid19.xlsx", 
+                    sheet = "Timeline", col_types = c("date", 
+                                                      "numeric", "text", "text", "text", "text", 
+                                                      "text", "text", "text", "text", "text", 
+                                                      "text", "text", "text", "text", "text", 
+                                                      "text", "text", "text", "text", "text", 
+                                                      "text", "text", "text", "text", "text", 
+                                                      "text", "text", "text", "text", "text", 
+                                                      "text", "text", "text", "text", "text"))
+
+
+# simpan semua datasets yang ada
+save(data_dunia,
+     file = '~/Documents/belajaR/Bukan Infografis/Shiny COVID REAL/realshinycovid/all files.rda')
