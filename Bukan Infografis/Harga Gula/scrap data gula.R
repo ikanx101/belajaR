6d2a4f4a0d3ed52601687739f29a63462c8711ac
ikanx101 <ikanx101@gmail.com>
2020-06-29 14:59:50 +0700

    gula

--- Bukan Infografis/Harga Gula/scrap data gula.R
+++ Bukan Infografis/Harga Gula/scrap data gula.R
@@ -1,6 +1,8 @@
 rm(list=ls())
 library(rvest)
-setwd('D:/Project_R/Harga Gula')
+setwd("~/Documents/belajaR/Bukan Infografis/Harga Gula")
+
+# data 3 tahun belakang
 tahun = c(2017,2018,2019)
 bulan = c('01','02','03','04','05','06','07','08','09','10','11','12')
 z = 1
@@ -13,6 +15,24 @@ for(i in 1:3){
   }
 }
 
+url_final = url
+
+# data tahun 2020 only (sampai bulan 05)
+
+tahun = c(2020)
+bulan = c('01','02','03','04','05')
+z = 1
+url = c()
+for(i in 1:1){
+  for(j in 1:5){
+    url[z] = paste('https://www.isosugar.org/prices.php?pricerange=',
+                   tahun[i],'-',bulan[j],'-01',sep='')
+    z = z+1
+  }
+}
+
+url_final = c(url_final,url)
+
 gula = function(url){
   data = read_html(url) %>% html_table(fill=T)
   data = data[[1]]
@@ -20,10 +40,10 @@ gula = function(url){
 }
 
 i = 1
-gula_1 = gula(url[i])
+gula_1 = gula(url_final[i])
 
-for(i in 2:36){
-  temp = gula(url[i])
+for(i in 2:41){
+  temp = gula(url_final[i])
   gula_1 = rbind(gula_1,temp)
 }
 
