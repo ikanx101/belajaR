f7488c31476adbd1c386e3c9199a6ffece5d1794
ikanx101 <ikanx101@gmail.com>
2020-10-21 17:06:13 +0700

    update

--- Bukan Infografis/tokopedia nyam/scrap topeds.R
+++ Bukan Infografis/tokopedia nyam/scrap topeds.R
@@ -13,7 +13,7 @@ library(dplyr)
 # input loc
 #ini aja nanti yang diganti
 input_loc = "~/belajaR/Bukan Infografis/tokopedia nyam/Link/"
-nama_file = "21 Oktober 2020 minuman" 
+nama_file = "21 Oktober 2020 flash 15" 
 
 # ambil dbase links 
 url = paste0(input_loc,nama_file,".txt")
