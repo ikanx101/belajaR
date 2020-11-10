21d9777aacdd6ca001684f2a9a15889e06a96e80
ikanx101 <ikanx101@gmail.com>
2020-11-10 20:47:12 +0700

    new

--- Bukan Infografis/CEO Talks/blog.R
+++ Bukan Infografis/CEO Talks/blog.R
@@ -15,6 +15,16 @@ blog = data.frame(id = 1,url = blog)
 blog = 
   blog %>%
   filter(grepl("2020",url)) %>% 
+  filter(!grepl("files",url)) %>% 
   distinct()
 
-blog
+blog = blog[-21:-25,]
+
+scrape = function(url){
+  url %>% 
+    read_html() %>% 
+    html_nodes("p") %>% 
+    html_text()
+}
+
+blog$isi = sapply(blog$url,scrape)
