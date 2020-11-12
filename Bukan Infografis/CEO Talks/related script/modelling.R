76633a2d70dc9078b4ec1732fa0582b8250b8fc3
ikanx101 <ikanx101@gmail.com>
2020-11-12 07:59:43 +0700

    model

--- Bukan Infografis/CEO Talks/related script/modelling.R
+++ Bukan Infografis/CEO Talks/related script/modelling.R
@@ -17,11 +17,25 @@ library(e1071)
 library(gmodels)
 
 set.seed(10104074)
-id_train = sample(176,145,replace = FALSE)
-train_data = data[id_train,]
+id_train_ceo = sample(72,60,replace = FALSE)
+id_train_bukan = sample(104,60,replace = FALSE)
+
+# dipisah dua
+data_ceo = 
+  data %>% 
+  filter(label == "CEO")
+data_bukan = 
+  data %>% 
+  filter(label != "CEO")
+
+train_data = rbind(data_ceo[id_train_ceo,],
+                   data_bukan[id_train_bukan,])
 train_data$label = factor(train_data$label)
 table(train_data$label)
-test_data = data[-id_train,]
+
+test_data = rbind(data_ceo[-id_train_ceo,],
+                  data_bukan[-id_train_bukan,])
+train_data$label = factor(train_data$label)
 test_data$label = factor(test_data$label)
 table(test_data$label)
 
@@ -72,4 +86,26 @@ mean(actual==predict)
 new = predict(sms_classifier_train2, sms_test,type = "raw")
 head(new)
 
-save(sms_classifier_train2,file = "model.rda")
+save(sms_classifier_train2,convert_counts,file = "model.rda")
+
+# ================
+rm(list=ls())
+load("model.rda")
+text = c("leader ceo covid innovation",
+         "person year call risk")
+coba = VCorpus(VectorSource(text))
+
+sms_corpus_clean.coba = tm_map(coba, content_transformer(tolower))
+sms_corpus_clean.coba = tm_map(sms_corpus_clean.coba, removeNumbers)
+sms_corpus_clean.coba = tm_map(sms_corpus_clean.coba, removePunctuation)
+sms_corpus_clean.coba = tm_map(sms_corpus_clean.coba, stripWhitespace)
+
+sms_dtm.coba = DocumentTermMatrix(sms_corpus_clean.coba)
+
+sms_freq_terms.coba <- findFreqTerms(sms_dtm.coba, 1)
+sms_data_freq_coba <- sms_dtm.coba[ , sms_freq_terms.coba]
+
+sms_coba <- apply(sms_data_freq_coba, MARGIN = 2, convert_counts)
+
+predict(sms_classifier_train2, sms_coba,type = "raw")
+
