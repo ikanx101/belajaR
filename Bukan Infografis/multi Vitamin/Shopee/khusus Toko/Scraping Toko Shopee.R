18a31de67c0d8d54cf47a7c0f695e9c54c7ca4e3
ikanx101 <ikanx101@gmail.com>
2021-01-27 14:40:21 +0700

    updates

--- Bukan Infografis/multi Vitamin/Shopee/khusus Toko/Scraping Toko Shopee.R
+++ Bukan Infografis/multi Vitamin/Shopee/khusus Toko/Scraping Toko Shopee.R
@@ -10,31 +10,25 @@ ambil_toko = function(url){
   tes = read_json(url)
   data = data.frame(
     nama_toko = tes$data$name,
-    lokasi = tes$data$place,
-    banyak_item_sold = tes$data$item_count,
-    official = tes$data$is_official_shop,
-    rating = tes$data$rating_star)
+    lokasi = tes$data$place)
   return(data)
 }
 
 i = 1
 data = ambil_toko(link_new[i])
 
-for(i in 1837:length(link_new)){
+for(i in 2:length(link_new)){
   temp = tryCatch({ambil_toko(link_new[i])},
                   error = function(e){
                     temp = data.frame(nama_toko = NA,
-                                      lokasi = NA,
-                                      banyak_item_sold = NA,
-                                      official = NA,
-                                      rating = NA)
+                                      lokasi = NA)
                   })
   data = rbind(data,temp)
   print(paste0('Alhamdulillah ',i,' sudah didapatkan...'))
 }
 
 
-data$link = link
+data$link = url
 data$link_new = link_new
 
 data_toko_soapy = data
